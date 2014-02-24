8a954fa7e0147bcd3c7b5571576d1cdb8d2025e6
Ross-Kitsis <ross.kitsis@gmail.com>
2014-02-24 01:41:25 -0500

    board clear bug fix

--- client/Model.java
+++ client/Model.java
@@ -425,5 +425,12 @@ public class Model
 	{
 		this.opponent = null;
 		this.haveGame = false;
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				board[i][j] = 0;
+			}
+		}
 	}
 }
